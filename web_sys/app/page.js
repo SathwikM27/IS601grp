8949a5cce07150aba55d893601ca9fa93afa0d32
SathwikM <sathwikdev27@gmail.com>
2023-12-17 15:47:18 -0500

    added 'featuring' section closes #40

--- web_sys/app/page.js
+++ web_sys/app/page.js
@@ -16,9 +16,11 @@ import MenuItem from '@mui/material/MenuItem';
 import Grid from '@mui/material/Grid';
 import MenuIcon from '@mui/icons-material/Menu';
 import Link from '@mui/material/Link';
-
-
-
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 
 const pages = [
@@ -192,6 +194,43 @@ const AboutUsSection = () => {
   );
 };
 
+const DishSlider = () => {
+  // Settings for the slider
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    nextArrow: <IconButton size= "large"><ArrowForwardIosIcon /></IconButton>,
+    prevArrow: <IconButton size= "large"><ArrowBackIosIcon /></IconButton>,
+  };
+
+  return (
+    <Box sx={{ maxWidth: '100%', padding: 2, mx: 'auto' }}>
+      <Typography variant="h4" gutterBottom sx={{ textAlign: 'center' }}>
+        Featuring
+      </Typography>
+      <Slider {...settings}>
+        {/* Map through your dishes here */}
+        <Box  sx={{ padding: '20px' }}>
+          <img src="/images/soup-dish.png" alt="Soups" style={{ width: '100%', height: 'auto' }} />
+          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>Soups</Typography>
+        </Box>
+        <Box  sx={{ padding: '20px' }}>
+          <img src="/images/chicken-dish.png" alt="Beef & Chicken" style={{ width: '100%', height: 'auto' }} />
+          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>Beef & Chicken</Typography>
+        </Box>
+        <Box  sx={{ padding: '20px' }}>
+          <img src="/images/dish1.jpg" alt="Vegetarian" style={{ width: '100%', height: 'auto' }} />
+          <Typography variant="subtitle1" sx={{ textAlign: 'center' }}>Vegetarian</Typography>
+        </Box>
+        {/* Add more slides as needed */}
+      </Slider>
+    </Box>
+  );
+};
+
 const HomePage = () => {
   return (
     <>
@@ -202,6 +241,7 @@ const HomePage = () => {
       <ResponsiveAppBar />
       <MainPageContent />
       <AboutUsSection />
+      <DishSlider />
       {/* Other components like footer, etc. */}
     </>
   );
