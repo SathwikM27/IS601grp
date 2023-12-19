9f088eea0caadf20871f6becb31cf419330545f8
SathwikM <sathwikdev27@gmail.com>
2023-12-19 06:24:48 -0500

    Fixed background Image Sizing issues closes #68

--- web_sys/pages/_document.js
+++ web_sys/pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en"> {/* Add whichever language you want here */}
       <Head />
-      <body>
+      <body style={{margin: 0}}>
         <Main />
         <NextScript />
       </body>
